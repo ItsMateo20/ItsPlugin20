04d92a95e6369e397cd749ba42275fb20dfd1819
ItsMateo20 <mateostar2009@gmail.com>
2022-01-19 13:55:27 +0100

    yeshaha

--- src/com/itsmateo20/Msg.java
+++ src/com/itsmateo20/Msg.java
@@ -7,7 +7,7 @@ public class Msg {
     public static void send(CommandSender sender, String message) {
         send(sender, message, "&a");
     }
-
+    
     public static void send(CommandSender sender, String message, String prefix) {
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
     }
